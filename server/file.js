9d66a195d25bc4e0054bbeee4f164c69dbfd0ece
韩非 - Fei Han <fei.han@xtalpi.com>
2021-08-30 18:33:03 +0800

    key-content map自动生成

--- server/file.js
+++ server/file.js
@@ -8,5 +8,22 @@ $('h2.title').text('Hello there!');
 $('h2').addClass('welcome');
 
 // $.html();
+// console.log($('.cms-editable'))
+// $('.cms-editable').text('1111111')
+// $('.cms-editable').text('1111111')
+// console.log()
 
-console.log($('.cms-editable').text())
+// fs.writeFileSync(path.resolve(__dirname, '../test.html'), $.html(), { encoding:'utf-8'})
+
+const json = {}
+$('.cms-editable').each((index,node)=>{
+    // console.log(node.name, $(node).text().slice(0,20).replace(/\s/g,''))
+    const text = $(node).text()
+    json[`${node.name}-${index}`] = text
+
+    console.log(node)
+})
+
+fs.writeFileSync(path.resolve(__dirname, '../test.json'), JSON.stringify(json), { encoding:'utf-8'})
+// console.log(json)
+
